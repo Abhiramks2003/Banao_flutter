11098562e98431c2fd6052f565cce8daee2b2abf
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2024-03-19 22:27:41 +0530

    updated bottombar

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -1,71 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Bottombar {
+  static BottomNavigationBarItem menuItems(String image, String label) {
+    return BottomNavigationBarItem(
+      activeIcon: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Image.asset(
+          image,
+          height: 25,
+          color: const Color(0xFF598BED),
+        ),
+      ),
+      icon: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Image.asset(
+          image,
+          height: 25,
+          color: const Color(0xFF939CA3),
+        ),
+      ),
+      label: label,
+    );
+  }
+
   static List<BottomNavigationBarItem> bottomBarItems(int currentPageIndex) {
     return <BottomNavigationBarItem>[
+      menuItems('lib/icons/homepage.png', 'Home'),
+      menuItems('lib/icons/book.png', 'Learn'),
+      menuItems('lib/icons/hub.png', 'Hub'),
+      menuItems('lib/icons/chat.png', 'Chat'),
       BottomNavigationBarItem(
-        activeIcon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/homepage.png',
-            height: 25,
-            color: const Color(0xFF598BED),
+          icon: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Image.asset('lib/icons/profile.png', height: 25),
           ),
-        ),
-        icon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/homepage.png',
-            height: 25,
-            color: const Color(0xFF939CA3),
-          ),
-        ),
-        label: 'Home',
-      ),
-      BottomNavigationBarItem(
-        activeIcon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/book.png',
-            height: 25,
-            color: const Color(0xFF598BED),
-          ),
-        ),
-        icon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/book.png',
-            height: 25,
-          ),
-        ),
-        label: 'Learn',
-      ),
-      BottomNavigationBarItem(
-        icon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/hub.png',
-            height: 25,
-          ),
-        ),
-        label: 'Hub',
-      ),
-      BottomNavigationBarItem(
-        icon: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Image.asset(
-            'lib/icons/chat.png',
-            height: 25,
-          ),
-        ),
-        label: 'Chat',
-      ),
-      const BottomNavigationBarItem(
-        icon: Icon(
-          Icons.person,
-        ),
-        label: 'Profile',
-      ),
+          label: 'Profile')
     ];
   }
 }
